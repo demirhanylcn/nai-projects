c68fa395ce6ac549441b6a023b0005d7594fd599
demjrhan <demirhanylcn@gmail.com>
2024-03-11 21:32:32 +0100

    need to find k times smallest

--- project1/main.py
+++ project1/main.py
@@ -21,24 +21,23 @@ The program should be written independently. It is not possible to use ready-mad
 The code (only the file with code not the whole project) should be placed in the appropriate task on the teams platform before classes and presented during classes.
 Only programs that meet the described requirements will be checked!"""
 import math
+import sys
 """
 We need to check if the given file is having right format of content.
 """
 
-def is_file_ok(test_file_path, train_file_path):
+"""def is_file_ok(test_file_path, train_file_path):
     with open(test_file_path, 'r') as test_file, open(train_file_path, 'r') as train_file:
         for line_test, line_train in zip(test_file, train_file):
-            content_test = line_test.strip().split(",")
-            content_train = line_train.strip().split(",")
-            if len(content_train) != 4 or not \
-                    (content_train[0].isdigit() and content_train[1].isdigit() and content_train[2].isdigit() and
-                    content_train[3].isalpha()):
+            content_test = line_test.split(",")
+            content_train = line_train.split(",")
+            if len(content_train) != 5:
                 raise ValueError(f"The file '{train_file_path}' doesn't contain the valid attributes.")
-            if len(content_test) != 4 or not (
+"""            """if len(content_test) != 5 or not (
                     content_test[0].isdigit() and content_test[1].isdigit() and content_test[2].isdigit() and
-                    content_test[3].isalpha()):
+                    content_train[3].isdigit() and content_train[4].isalpha()):
                 raise ValueError(f"The file '{test_file_path}' doesn't contain the valid attributes.")
-
+"""
 
 """
 We will open both of the files and do the calculations with given number of K.
@@ -49,50 +48,65 @@ If so we will determine if it is true or not then return it.
 
 
 def calculations(train_file_path, test_file_path, given_k):
-    distances = []
+
     final_name_of_classes = []
-    classes = {}
+
+
     try:
-        is_file_ok(test_file_path, train_file_path)
         with open(test_file_path, 'r') as file_test:
             test_file_content = file_test.readlines()
         with open(train_file_path, 'r') as file_train:
             for i in range(0, len(test_file_content)):
-                for line in file_train:
-                    content = line.split(",")
-                    first_value = (float(content[0]) - float(test_file_content[i][0])) ** 2
-                    second_value = (float(content[1]) - float(test_file_content[i][1])) ** 2
-                    third_value = (float(content[2]) - float(test_file_content[i][2])) ** 2
-                    distance = math.sqrt(first_value + second_value + third_value)
-                    distances.append(str(distance) + ":" + str(content[3]))
-                distances.sort()
-                for each in range(0, len(given_k)):
-                    _, name_of_class = str(each.split(":"))
-                    if name_of_class in classes:
-                        classes[name_of_class] += 1
+                distances = []
+                test_content = []
+                test_raw = test_file_content[i].strip().split(",")
+                for k in range(0,len(test_raw)):
+                    if k == len(test_raw) - 1:
+                        test_content.append(str(test_raw[k]))
                     else:
-                        classes[name_of_class] = 1
-                majority_class = max(classes, key=classes.get)
-                final_name_of_classes.append(majority_class)
-                classes.clear()
-                distances.clear()
+                        test_content.append(float(test_raw[k]))
+                for line in file_train:
+                    train_content = []
+                    train_raw = line.split(",")
+                    for j in range(0, len(train_raw)):
+                        if j == len(train_raw) - 1:
+                            train_content.append(str(train_raw[j]))
+                        else:
+                            train_content.append(float(train_raw[j]))
+                    first_value = (train_content[0] - test_content[0]) ** 2
+                    second_value = (train_content[1] - test_content[1]) ** 2
+                    third_value = (train_content[2] - test_content[2]) ** 2
+                    forth_value = (train_content[3] - test_content[3]) ** 2
+                    distance = math.sqrt(first_value + second_value + third_value + forth_value)
+                    """need to find smallest k times in distances and the most repeated class name."""
+
+
+
+
+
+
+
         return final_name_of_classes
 
     except Exception as e:
         print(e)
 
-
+training_file = "/Users/demjrhan/Documents/nai-projects/project1/train.txt"
 while True:
     print("A) use the test file \n"
           "B) provide your own test file \n"
           "C) exit the program. \n")
-    user_input = input(str(input("enter a,b or c.")))
+    user_input = str(input("enter a,b or c."))
     match user_input:
         case "a":
-            training_file_path_a = str(input("enter training file path"))
-            number_k_a = int(input("enter the K number"))
+            test_file = str(input("enter test file path"))
+            k_number = int(input("enter k"))
+            calculations(training_file,test_file,k_number)
+
+            break
         case "b":
-            training_file_path_b = str(input("enter training file path"))
+            training_file_path_b = str(input("enter test file path"))
             number_k_b = int(input("enter the K number"))
+            break
         case "c":
-            SystemExit
+            sys.exit()
