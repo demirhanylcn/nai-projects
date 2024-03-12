c8cb1dc9808b1511e44584feaef3980dc13d68ee
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2024-03-12 17:48:34 +0100

    finished.

--- project1/main.py
+++ project1/main.py
@@ -22,46 +22,73 @@ Only programs that meet the described requirements will be checked!"""
 import math
 import sys
 
-"""
-We need to check if the given file is having right format of content.
-"""
 
-"""def is_file_ok(test_file_path, train_file_path):
+def is_file_ok(test_file_path, train_file_path):
     with open(test_file_path, 'r') as test_file, open(train_file_path, 'r') as train_file:
         for line_test, line_train in zip(test_file, train_file):
-            content_test = line_test.split(",")
-            content_train = line_train.split(",")
-            if len(content_train) != 5:
-                raise ValueError(f"The file '{train_file_path}' doesn't contain the valid attributes.")
-"""            """if len(content_test) != 5 or not (
-                    content_test[0].isdigit() and content_test[1].isdigit() and content_test[2].isdigit() and
-                    content_train[3].isdigit() and content_train[4].isalpha()):
-                raise ValueError(f"The file '{test_file_path}' doesn't contain the valid attributes.")
-"""
+            content_test_raw = line_test.strip().split(",")
+            content_train_raw = line_train.strip().split(",")
 
-"""
-We will open both of the files and do the calculations with given number of K.
-We will make calculations and put them into list
-Depending on the given value of K we will check the main class is matching with the test class.
-If so we will determine if it is true or not then return it.
-"""
+            content_test_calculated = []
+            for i in range(len(content_test_raw)):
+                if i == len(content_test_raw) - 1:
+                    content_test_calculated.append(str(content_test_raw[i]))
+                else:
+                    content_test_calculated.append(float(content_test_raw[i]))
+
+            content_train_calculated = []
+            for j in range(len(content_train_raw)):
+                if j == len(content_train_raw) - 1:
+                    content_train_calculated.append(str(content_train_raw[j]))
+                else:
+                    content_train_calculated.append(float(content_train_raw[j]))
+
+
+
+            if len(content_train_calculated) != len(content_test_calculated):
+                raise ValueError(
+                    f"The files '{test_file_path}' and '{train_file_path}' don't contain the same number of attributes.")
+
+            for test_value in content_test_calculated[:-1]:
+                if not isinstance(test_value, float):
+                    raise ValueError(f"The file '{test_file_path}' doesn't contain valid numerical attributes.")
+
+            for train_value in content_train_calculated[:-1]:
+                if not isinstance(train_value, float):
+                    raise ValueError(f"The file '{train_file_path}' doesn't contain valid numerical attributes.")
+
+            if not isinstance(content_test_calculated[-1], str):
+                raise ValueError(f"The file '{test_file_path}' doesn't contain a valid attribute as the last element.")
+
+            if not isinstance(content_train_calculated[-1], str):
+                raise ValueError(f"The file '{train_file_path}' doesn't contain a valid attribute as the last element.")
 
 
 def main_file(train_file_path, test_file_path, given_k):
-    with open(test_file_path, 'r') as file_test:
-        test_file_content = file_test.readlines()
-    with open(train_file_path, 'r') as file_train:
-        train_file_content = file_train.readlines()
 
-    final_answers = []
 
-    for test_file_count in range(len(test_file_content)):
-        distances_names = find_distances_classes(test_file_content, train_file_content,test_file_count)
-        final_class = find_final_class(distances_names, given_k)
-        answer = check_correctness_of_classes(final_class, test_file_count,test_file_content)
-        final_answers.append(answer)
+    try:
 
-    return final_answers
+        is_file_ok(test_file_path, train_file_path)
+        with open(test_file_path, 'r') as file_test:
+            test_file_content = file_test.readlines()
+        with open(train_file_path, 'r') as file_train:
+            train_file_content = file_train.readlines()
+
+        final_answers = []
+
+        for test_file_count in range(0, len(test_file_content)):
+            distances_names = find_distances_classes(test_file_content, train_file_content, test_file_count)
+            print(sorted(distances_names.copy()))
+            final_class = find_final_class(distances_names, given_k)
+            answer = check_correctness_of_classes(final_class, test_file_count, test_file_content)
+            final_answers.append(answer)
+
+        return final_answers
+    except ValueError as e:
+        print(e)
+
+
 
 
 def find_distances_classes(test_file_content, train_file_content, test_file_count):
@@ -85,31 +112,34 @@ def find_distances_classes(test_file_content, train_file_content, test_file_coun
             else:
                 train_file_content_calculated.append(float(train_file_line_raw_content[j]))
 
-        first_value = (train_file_content_calculated[0] - test_file_content_calculated[0]) ** 2
-        second_value = (train_file_content_calculated[1] - test_file_content_calculated[1]) ** 2
-        third_value = (train_file_content_calculated[2] - test_file_content_calculated[2]) ** 2
-        forth_value = (train_file_content_calculated[3] - test_file_content_calculated[3]) ** 2
-        distance = math.sqrt(first_value + second_value + third_value + forth_value)
-        distances_classes.append(str(distance) + ":" + str(train_file_content_calculated[4]))
+        calculations = []
+        total_sum = 0
+        for k in range(len(test_file_content_calculated)-1):
+            value = (train_file_content_calculated[k] - test_file_content_calculated[k]) ** 2
+            calculations.append(value)
+        for value in calculations:
+            total_sum += value
+        distance = math.sqrt(total_sum)
+        distances_classes.append(str(distance) + ":" + str(train_file_content_calculated[len(train_file_content_calculated) - 1]))
 
     return distances_classes
 
+
 def find_minimum_index(distance_list, given_k):
     min_index_list = []
+    distance_list_copy = distance_list.copy()
 
-    for k in range(given_k):
-        min_number = distance_list[0]
-        min_index = 0
-
-        for i in range(1, len(distance_list)):
-            if distance_list[i] < min_number:
-                min_number = distance_list[i]
-                min_index = i
+    for i in range(given_k):
+        min_number = min(distance_list_copy)
+        min_index = distance_list_copy.index(min_number)
 
         min_index_list.append(min_index)
+        distance_list_copy[min_index] = float('inf')
 
     return min_index_list
-def find_final_class(distances_classes,given_k):
+
+
+def find_final_class(distances_classes, given_k):
     distances = []
     class_names = []
     for i in range(len(distances_classes)):
@@ -119,11 +149,12 @@ def find_final_class(distances_classes,given_k):
         distances.append(distance)
         class_names.append(class_name)
 
-    minimum_indexes = find_minimum_index(distances,given_k)
+    minimum_indexes = find_minimum_index(distances, given_k)
     final_class_names = []
-    for i in range(len(class_names)):
-        if i in minimum_indexes:
-            final_class_names.append(class_names[i])
+
+    for j in range(len(minimum_indexes)):
+        final_class = class_names[minimum_indexes[j]]
+        final_class_names.append(final_class)
 
     final_class_dictionary = {}
     for name in final_class_names:
@@ -131,17 +162,27 @@ def find_final_class(distances_classes,given_k):
             final_class_dictionary[name] += 1
         else:
             final_class_dictionary[name] = 1
-    most_repeated_name = max(final_class_dictionary, key=final_class_dictionary.get)
+
+    most_repeated_name = None
+    max_count = 0
+    for name, count in final_class_dictionary.items():
+        if count > max_count:
+            most_repeated_name = name
+            max_count = count
+
     return most_repeated_name
 
-def check_correctness_of_classes(final_class,test_file_count,test_file_content):
+
+def check_correctness_of_classes(final_class, test_file_count, test_file_content):
     test_file_line_list = test_file_content[test_file_count].strip().split(",")
-    test_file_class_name = test_file_line_list[len(test_file_line_list)-1]
-    print(test_file_class_name,final_class)
+    test_file_class_name = test_file_line_list[len(test_file_line_list) - 1]
+    print("test_file_class_name = ", test_file_class_name)
+    print("final_class = ", final_class)
     if test_file_class_name == final_class:
         return True
     else:
         return False
+
 
 training_file = "C:\\Users\\demir\\Documents\\git\\nai-projects\\project1\\train.txt"
 test_file = "C:\\Users\\demir\\Documents\\git\\nai-projects\\project1\\test.txt"
@@ -153,15 +194,16 @@ while True:
     user_input = str(input("enter a,b or c."))
     match user_input.lower().strip()[0]:
         case "a":
-            test_file = test_file
+            test_file_path = test_file
             k_number = int(input("enter k"))
             answers = main_file(training_file, test_file, k_number)
             print(answers)
 
-            break
         case "b":
-            training_file_path_b = str(input("enter test file path"))
-            number_k_b = int(input("enter the K number"))
-            break
+            test_file_path = str(input("enter test file path"))
+            k_number = int(input("enter the K number"))
+            answers = main_file(training_file, test_file_path, k_number)
+            print(answers)
+
         case "c":
             sys.exit()
