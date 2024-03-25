f0d6466092dcd6a66955d6be27655fdffe1de25f
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2024-03-25 12:34:11 +0100

    added basic methods

--- project2/main.py
+++ project2/main.py
@@ -7,18 +7,26 @@ def initializeThreshold():
     return threshold
 
 def initializeWeights(size):
-    weights = []
+    weightList = []
     for i in range(size-1):
         random_number = random.uniform(0.1, 1)
         weight = round(random_number, 1) * 0.5
-        weights.append(weight)
+        weightList.append(weight)
+    return weightList
+
+def getContentOfFile(filePath):
+    with open(filePath, 'r') as file:
+        content = file.readlines()
+    return content
 
 """dot product : testfile each * weights each,
  in the end answer is bigger than threshold y is 1 otherwise 0."""
 """ delta rule = weights old + ( last element of the line + y )
  * learning rate * rest elements of file"""
-def dotProduct():
-
+def dotProduct(testFileLine, weightList,threshold):
+    calculations = []
+    for i,y in range(len(testFileLine)-1):
+        testFile = testFileLine[i]
 
 
 def main_method():
