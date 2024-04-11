7c1ba28a7ab259515e66d7d85d741569a5e2cfe4
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2024-04-11 12:50:01 +0200

    output is not right

--- project3/main.py
+++ project3/main.py
@@ -11,71 +11,50 @@ class Neuron:
     def initalize_weights(self, size):
         weightList = []
         for i in range(size):
-            random_number = random.uniform(0.5, 1)
+            random_number = random.uniform(0.2, 1)
             weight = round(random_number, 1) * 0.7
             weightList.append(round(weight, 2))
         return weightList
 
-    def train(self, learning_rate=0.1, epochs=1000):
-        # Loop through each epoch
+    def train(self, learning_rate, epochs):
         for epoch in range(epochs):
-            # Loop through each language vector
-            for lang_name in self.language_vectors:
-                lang_vector = self.language_vectors[lang_name]
+            for lang_name in self.languages:
                 lang_index = self.get_lang_index(lang_name)
-
-                # Calculate the net value for each weight vector
-                net_values = []
-                for weights in self.weights:
-                    net_value = 0
-                    for i in range(len(weights)):
-                        net_value += lang_vector[i] * weights[i]
-                    net_values.append(net_value)
-
-
-                # Update the weights
                 for i in range(len(self.weights)):
-                    # If the weight vector corresponds to the current language vector
-                    if i == lang_index:
-                        # Update the weights to move closer to 1
-                        self.update_weights(i, learning_rate, direction='increase')
-                    else:
-                        # Update the weights to move closer to 0
-                        self.update_weights(i, learning_rate, direction='decrease')
+                    input_vector = self.language_vectors[lang_name]
+                    is_correct_language = (i == lang_index)
+                    self.update_weights(i, learning_rate, input_vector, is_correct_language)
 
     def get_lang_index(self, lang_name):
-        # Get the index of the language vector
+
         for index in range(len(self.languages)):
             if self.languages[index] == lang_name:
                 return index
 
-    def update_weights(self, index, learning_rate, direction='increase'):
-        # Update the weights based on the specified direction
+    def update_weights(self, index, learning_rate, input_vector, is_correct_language):
         for j in range(len(self.weights[index])):
-            if direction == 'increase':
-                # Move the weight closer to 1
-                self.weights[index][j] += learning_rate * (1 - self.weights[index][j])
-            elif direction == 'decrease':
-                # Move the weight closer to 0
-                self.weights[index][j] -= learning_rate * self.weights[index][j]
+            if is_correct_language:
+                self.weights[index][j] += learning_rate * input_vector[j]
+            else:
+                self.weights[index][j] -= learning_rate * input_vector[j]
 
     def load_data(self, data_dir):
         for lang_folder in os.listdir(data_dir):
             self.languages.append(lang_folder)
             lang_path = os.path.join(data_dir, lang_folder)
-            lang_vector = [0] * 128  # ASCII characters
+            lang_vector = [0] * 128
             total_files = 0
             for file_name in os.listdir(lang_path):
                 total_files += 1
                 with open(os.path.join(lang_path, file_name), 'r', encoding='utf-8') as file:
                     text = file.read()
                     for char in text:
-                        if ord(char) < 128:  # considering only ASCII characters
-                            lang_vector[ord(char)] += 1/9
-            self.language_vectors[lang_folder] = [freq / total_files for freq in lang_vector]
+                        if ord(char) < 128:
+                            lang_vector[ord(char)] += 1
+            self.language_vectors[lang_folder] = [freq / 128 for freq in lang_vector]
 
     def classify(self, text):
-        input_vector = [0] * 128  # Initialize input vector for ASCII characters
+        input_vector = [0] * 128
         for char in text:
             if ord(char) < 128:
                 input_vector[ord(char)] += 1
@@ -86,9 +65,9 @@ class Neuron:
             for i in range(len(input_vector)):
                 net_value += input_vector[i] * weights[i]
             net.append(net_value)
-
-        max_index = net.index(max(net))  # Find the index of the maximum net value
-        return self.languages[max_index]  # Return the language corresponding to the maximum net value
+        print(net)
+        max_index = net.index(max(net))
+        return self.languages[max_index]
 
 
 if __name__ == '__main__':
@@ -96,10 +75,10 @@ if __name__ == '__main__':
     classifier.load_data("C:\\Users\\demir\\Documents\\git\\nai-projects\\project3\\languages")
     num_languages = len(classifier.languages)
     classifier.weights = [classifier.initalize_weights(128) for _ in range(num_languages)]
-    classifier.train()
+    epoch = int(input("Enter number of epochs: "))
+    learning_rate = float(input("Enter learning rate: "))
+    classifier.train(learning_rate, epoch)
     while True:
-        text = input("Enter text to classify: ")
-        if text.lower() == "exit":
-            break
+        text = input("Enter the sentence: ")
         language = classifier.classify(text)
         print("Predicted language:", language)
