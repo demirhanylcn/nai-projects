f25da755024e6133aff373957527d7bd6140345e
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2024-04-11 13:23:53 +0200

    output is weird but okay

--- project3/main.py
+++ project3/main.py
@@ -11,7 +11,7 @@ class Neuron:
     def initalize_weights(self, size):
         weightList = []
         for i in range(size):
-            random_number = random.uniform(0.2, 1)
+            random_number = random.uniform(0.2, 0.5)
             weight = round(random_number, 1) * 0.7
             weightList.append(round(weight, 2))
         return weightList
@@ -19,13 +19,13 @@ class Neuron:
     def train(self, learning_rate, epochs):
         for epoch in range(epochs):
             for lang_name in self.languages:
-                lang_index = self.get_lang_index(lang_name)
+                lang_index = self.get_language_index(lang_name)
                 for i in range(len(self.weights)):
                     input_vector = self.language_vectors[lang_name]
                     is_correct_language = (i == lang_index)
                     self.update_weights(i, learning_rate, input_vector, is_correct_language)
 
-    def get_lang_index(self, lang_name):
+    def get_language_index(self, lang_name):
 
         for index in range(len(self.languages)):
             if self.languages[index] == lang_name:
@@ -38,7 +38,7 @@ class Neuron:
             else:
                 self.weights[index][j] -= learning_rate * input_vector[j]
 
-    def load_data(self, data_dir):
+    def find_chars(self, data_dir):
         for lang_folder in os.listdir(data_dir):
             self.languages.append(lang_folder)
             lang_path = os.path.join(data_dir, lang_folder)
@@ -53,7 +53,7 @@ class Neuron:
                             lang_vector[ord(char)] += 1
             self.language_vectors[lang_folder] = [freq / 128 for freq in lang_vector]
 
-    def classify(self, text):
+    def prediction(self, text):
         input_vector = [0] * 128
         for char in text:
             if ord(char) < 128:
@@ -71,14 +71,14 @@ class Neuron:
 
 
 if __name__ == '__main__':
-    classifier = Neuron()
-    classifier.load_data("C:\\Users\\demir\\Documents\\git\\nai-projects\\project3\\languages")
-    num_languages = len(classifier.languages)
-    classifier.weights = [classifier.initalize_weights(128) for _ in range(num_languages)]
+    neuron = Neuron()
+    neuron.find_chars("C:\\Users\\demir\\Documents\\git\\nai-projects\\project3\\languages")
+    language_count = len(neuron.languages)
+    neuron.weights = [neuron.initalize_weights(128) for _ in range(language_count)]
     epoch = int(input("Enter number of epochs: "))
     learning_rate = float(input("Enter learning rate: "))
-    classifier.train(learning_rate, epoch)
+    neuron.train(learning_rate, epoch)
     while True:
         text = input("Enter the sentence: ")
-        language = classifier.classify(text)
+        language = neuron.prediction(text)
         print("Predicted language:", language)
